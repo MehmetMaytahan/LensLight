7325538b9effffe46da9f370be985567e1d028a9
Mehmet Maytahan <maytahan71@gmail.com>
2023-05-16 17:55:10 +0300

    User Model Oluşturuldu

---
+++ models/UserModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
