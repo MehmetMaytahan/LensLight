ccb3a117f4d51ff90a764291f89e4549fd08f70c
Mehmet Maytahan <maytahan71@gmail.com>
2023-05-26 17:49:34 +0300

    Fotoğraf yükleme işlemi yapıldı

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -3,10 +3,7 @@ import Photo from "../models/PhotoModel.js";
 const createPhoto = async (req, res) => {
     try {
         const photo = await Photo.create(req.body)
-        res.status(201).json({
-            succeded: true,
-            photo
-        })
+        res.status(201).redirect('/users/dashboard')
     } catch (error) {
         res.status(500).json({
             succeded: false,
