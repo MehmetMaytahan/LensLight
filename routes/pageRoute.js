a434deb84655bece4f91ab6c2f141721a3db55bc
Mehmet Maytahan <maytahan71@gmail.com>
2023-06-07 16:51:27 +0300

    mail gönderme işlemi gerçekleştirildi

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -10,5 +10,7 @@ router.get('/about', pageController.getAboutPage)
 router.get('/register', pageController.getRegisterPage)
 router.get('/login', pageController.getLoginPage)
 router.get('/logout', pageController.getLogout)
+router.post('/contact', pageController.sendMail)
+router.get('/contact', pageController.getContactPage)
 
 export default router
